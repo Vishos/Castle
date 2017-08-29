0148ed5439d038667b24265a538325793cb2ec97
Vishos <graham.layman@comcast.net>
2017-08-29 13:09:23 -0700

    initial commit

---
+++ castle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace castle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Splash.Title();
+            Console.WriteLine("Are you ready to play Castle?");
+            Console.WriteLine("[i] Instructions\n[p] Play");
+            string response = Console.ReadLine();
+            if (response == "i") {
+                //display instructions
+            }
+            else if (response == "p") {
+                bool keepPlaying = false;
+                while(keepPlaying){
+                    Game game = new Game();
+                    System.Console.WriteLine("Would you like to play again?");
+                    Console.Write("yes or no? >>");
+                    string keepPlayingResponse = Console.ReadLine();
+                    if(keepPlayingResponse == "yes"){
+                        keepPlaying = true;
+                    }
+                }
+                System.Console.WriteLine("Thank you for playing Castle. Goodbye.");
+              
+            }
+         
+        }
+    }
+}
